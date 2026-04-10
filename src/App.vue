6d4bd9f66d0803bc16cff7fd330207e34903eca0
wenping <wenping@meituan.com>
2026-04-10 10:09:53 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <n-button>naive-ui</n-button>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+</template>
+
+<script setup lang="ts">
+import { NButton } from 'naive-ui';
+</script>
